740a3c818db63fd68423dcae4fe2da05b2ed0f90
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-04-25 18:17:46 +0300

    add

---
+++ gatsby-shopify/src/pages/start.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout, SEO } from 'components';
+
+const StartPage = () => (
+  <Layout>
+    <SEO title="Start" />
+    <h1>yo</h1>
+    <p>Welcome to the start page</p>
+  </Layout>
+);
+
+export default StartPage;
