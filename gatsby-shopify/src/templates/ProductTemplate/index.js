9e7a5f9778c8de181ffd3be73c61fbed48892b3e
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-04-28 17:59:22 +0300

    add

--- gatsby-shopify/src/templates/ProductTemplate/index.js
+++ gatsby-shopify/src/templates/ProductTemplate/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import  {graphql}  from 'gatsby';
+// we used absolut path here :: look into onCreateWebpackConfig in gatsby-node.js
+import {Layout} from 'components';
+import {Grid} from './styles'
 
 
 export const query = graphql`
@@ -13,6 +16,15 @@ query ProductQuery($shopifyid: String){
 export default function ProductTemplate(props) {
     console.log(props);
         return (
-                <h1>{props.data.shopifyProduct.title}</h1>
+                <Layout>
+                <Grid>
+                   <div>
+                        <h1>{props.data.shopifyProduct.title}</h1>
+                   </div>
+                   <div>
+                           Image
+                   </div>
+                </Grid>
+                </Layout>
         )
 }
