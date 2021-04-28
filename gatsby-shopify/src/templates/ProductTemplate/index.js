1a3f9fdf417105be2e433cecaa13de92fa5d705b
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-04-28 17:37:13 +0300

    add

--- gatsby-shopify/src/templates/ProductTemplate/index.js
+++ gatsby-shopify/src/templates/ProductTemplate/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import  {graphql}  from 'gatsby';
+
+
+export const query = graphql`
+query ProductQuery($shopifyid: String){
+        shopifyProduct(shopifyId: {eq: $shopifyid}) {
+        title
+     }
+   }
+`;
 
 export default function ProductTemplate(props) {
     console.log(props);
         return (
-                <h1>hi</h1>
+                <h1>{props.data.shopifyProduct.title}</h1>
         )
 }
