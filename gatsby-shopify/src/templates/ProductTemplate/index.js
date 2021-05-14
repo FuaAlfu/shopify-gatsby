2a2da1c1608bbbcb9013a5f07044bf3edebbaff3
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-05-14 18:48:06 +0300

    add

--- gatsby-shopify/src/templates/ProductTemplate/index.js
+++ gatsby-shopify/src/templates/ProductTemplate/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-ally/no-onChange*/
 import React from 'react'
 import  {graphql}  from 'gatsby';
 // we used absolut path here :: look into onCreateWebpackConfig in gatsby-node.js
 import {Layout, ImageGallery} from 'components';
 import {Grid, SelectWrapper, Price} from './styles';
 import {CartContext} from 'context/CartContext';
+import {navigate, useLocatuion} from '@reach/router';
 
 /*
 to write a fragment:
@@ -34,16 +36,23 @@ export default function ProductTemplate(props) {
    const {getProductById} = React.useContext(CartContext);
    const [product, setProduct] = React.useState(null);
    const [selectedVariant, setSelectedVariant] = React.useState(null)
+   const {search, origin, pathname} = useLocatuion();
+   console.log(search, origin, pathname);
+
    React.useEffect(() =>{
      getProductById(props.data.shopifyProduct.shopifyId).then((result) =>{
        //console.log(result);
        setProduct(result);
        selectedVariant(result.variants[0]);
      })
-   },[getProductById, setProduct]);
+   },[getProductById, setProduct,props.data.shopifyProduct.shopifyId]);
 
    const handleVariantChange = (e) =>{
-     setSelectedVariant(product?.variant.find(v => v.id === e.target.value));
+     const newVariant = product?.variant.find(v => v.id === e.target.value);
+     setSelectedVariant(newVariant);
+     navigate(`${origin}${pathname}?variant=${encodeURIComponent(newVariant.id)}`,{
+        replace: true
+     })
    }
         return (
                 <Layout>
